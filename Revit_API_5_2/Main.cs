1272899b1dd0ca51b281b6233f537be37a2be0c8
cadman77 <cadman77@yandex.ru>
2022-03-25 16:40:44 +0300

    1ая попытка сдать на проверку

--- Revit_API_5_2/Main.cs
+++ Revit_API_5_2/Main.cs
@@ -7,7 +7,6 @@
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
-using Autodesk.Revit.UI.Selection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,52 +17,10 @@ namespace Revit_API_5_2
 
     public class Main : IExternalCommand
     {
-        public List<Element> SelectedWalls { get; set; }
-
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
-
-            UIApplication uiapp = commandData.Application;
-            UIDocument uidoc = uiapp.ActiveUIDocument;
-            Document doc = uidoc.Document;
-
-            try
-            {
-                IList<Reference> selectedWallRefList = uidoc.Selection.PickObjects(ObjectType.Element, new WallFilter(), "Выберите стены:");
-                #region // multi-line variant
-                //List<Wall> selectedWalls = new List<Wall>();
-
-                //foreach (Reference selectedRef in selectedWallRefList)
-                //{
-                //    Wall oWall = doc.GetElement(selectedRef) as Wall;
-                //    selectedWalls.Add(oWall);
-                //}
-
-                //List<Element> selectedWalls = selectedWallRefList.Select(selectedObject => doc.GetElement(selectedObject)).ToList();
-                //SelectedWalls = selectedWalls;
-                #endregion
-                SelectedWalls = selectedWallRefList.Select(selectedObject => doc.GetElement(selectedObject)).ToList();
-
-            }
-            catch (OperationCanceledException) // ??
-            {
-                //TaskDialog.Show("Отмена", "Команда была прервана пользователем.");
-                return Result.Cancelled;
-            }
-            catch (Exception ex)
-            {
-                //TaskDialog.Show("Ошибка", "При выполнении команды возникла непредвиденная ошибка.");
-                TaskDialog.Show("Ошибка", $"{ex.Message}");
-                return Result.Failed;
-            }
-
             MainView window = new MainView(commandData);
             window.ShowDialog();
-            //TaskDialog.Show("Завершено", $"Тип выбранных стен ({SelectedWalls.Count} шт.) успешно изменен на \"{SelectedWallTypeName}\".");
-            TaskDialog.Show("Завершено", $"Тип выбранных стен ({SelectedWalls.Count} шт.) успешно изменен.");
-
-            uidoc.Selection.Dispose();
-            uidoc.RefreshActiveView();
             return Result.Succeeded;
         }
     }
--- Revit_API_5_2/MainViewViewModel.cs
+++ Revit_API_5_2/MainViewViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
 using Prism.Commands;
 using System;
 using System.Collections.Generic;
@@ -11,56 +12,91 @@ namespace Revit_API_5_2
 {
     public class MainViewViewModel
     {
-        private ExternalCommandData CommandData;
-        private static Document Doc;
+        private ExternalCommandData _commandData;
+        private UIDocument _uidoc;
+        private Document _doc;
 
-        public List<string> WallTypeNames { get; set; }
-        
-        public string SelectedWallTypeName { get; set; }
+        public List<Wall> SelectedWalls { get; set; }
+        public List<WallType> ValidWallTypes { get; set; }
+        public WallType SelectedWallType { get; set; }
 
         public DelegateCommand SetWallTypeCommand { get; }
-
-        public event EventHandler CloseRequest;
-        
-        private void RaiseCloseRequest()
-        {
-            CloseRequest?.Invoke(this, EventArgs.Empty);
-        }
 
         public MainViewViewModel(ExternalCommandData commandData)
         {
 
-            //UIApplication uiapp = commandData.Application;
-            //UIDocument uidoc = uiapp.ActiveUIDocument;
-            //Document doc = uidoc.Document;
+            _commandData = commandData;
+            _uidoc = _commandData.Application.ActiveUIDocument;
+            _doc = _uidoc.Document;
 
-            CommandData = commandData;
-            Doc = CommandData.Application.ActiveUIDocument.Document;
-
-            List<WallType> wallTypes = new FilteredElementCollector(Doc)
+            List<WallType> validWallTypes = new FilteredElementCollector(_doc)
                 .OfCategory(BuiltInCategory.OST_Walls)
                 .WhereElementIsElementType()
                 .Cast<WallType>()
                 .ToList();
 
-            List<string> wallTypeNames = new List<string>();
+            ValidWallTypes = validWallTypes;
 
-            foreach (WallType wallType in wallTypes)
+            try
             {
-                wallTypeNames.Add(wallType.get_Parameter(BuiltInParameter.ALL_MODEL_TYPE_NAME).AsString());
+                IList<Reference> selectedWallRefList = _uidoc.Selection.PickObjects(ObjectType.Element, new WallFilter(), "Выберите стены:");
+
+                List<Wall> selectedWalls = new List<Wall>();
+
+                foreach (Reference selectedRef in selectedWallRefList)
+                {
+                    Wall wall = _doc.GetElement(selectedRef) as Wall;
+                    selectedWalls.Add(wall);
+                }
+
+                SelectedWalls = selectedWalls;
+
+                SetWallTypeCommand = new DelegateCommand(OnSetWallTypeCommand);
             }
-
-            WallTypeNames = wallTypeNames;
-
-            SetWallTypeCommand = new DelegateCommand(OnSetWallTypeCommand);
+            catch (Autodesk.Revit.Exceptions.OperationCanceledException) // ?? прервать дальнейшее выполнение команды!!
+            {
+                //TaskDialog.Show("Отмена", "Команда прервана пользователем.");
+                return;
+            }
+            catch (Exception ex) // ?? прервать дальнейшее выполнение команды!!
+            {
+                TaskDialog.Show("Ошибка", $"{ex.Message}");
+                return;
+            }            
         }
 
         private void OnSetWallTypeCommand()
         {
-            //if (SelectedWalls.Count == 0)
-            //{
+            if (SelectedWalls.Count == 0)
+            {
+                return;
+            }
 
-            //}
+            using (Transaction ts = new Transaction(_doc, "Set Wall Type Transaction"))
+            {
+                ts.Start();
+
+                foreach (Wall wall in SelectedWalls)
+                {
+                    wall.WallType = SelectedWallType;
+                }
+
+                ts.Commit();
+            }
+
+            RaiseCloseRequest();
+
+            TaskDialog.Show("Выполнено", $"Тип выбранных стен ({SelectedWalls.Count} шт.) успешно изменен на \"{SelectedWallType.Name}\".");
+
+            _uidoc.Selection.Dispose();
+            _uidoc.RefreshActiveView();
+        }
+
+        public event EventHandler CloseRequest;
+
+        private void RaiseCloseRequest()
+        {
+            CloseRequest?.Invoke(this, EventArgs.Empty);
         }
     }
 }
